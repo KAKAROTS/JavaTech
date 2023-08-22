d2b070785f420604b8d0837739b110e4174ecef5
xiongzixuan <xiongzixuan@lattebank.com>
2023-08-22 10:56:52 +0800

    java核心知识体系初始化

---
+++ src/main/java/com/dragonball/javatech/JavaTechApplication.java
@@ -0,0 +1,13 @@
+package com.dragonball.javatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTechApplication.class, args);
+    }
+
+}
